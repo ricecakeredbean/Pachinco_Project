c7700ac6111db26e9021b68b93451f63e4dcb36d
bch6224 <a01022105263@gmail.com>
2023-06-12 00:01:36 +0900

    2023/06/11 MapTag+

--- Assets/C#/MapSpawn/MapSpawnSystem.cs
+++ Assets/C#/MapSpawn/MapSpawnSystem.cs
@@ -12,24 +12,26 @@ public class MapSpawnSystem : MonoBehaviour
     private int mapVertiIndex = 0;
     private int mapMaxHorizonIndex = 4;
 
+    private Vector2 mapInterval;
+
     private HashSet<RectTransform> openHashSet = new();
 
     private void Start()
     {
         MapSpawn();
+        mapInterval = new Vector2(Screen.width / 6, content.sizeDelta.y / 9);
     }
 
     [ContextMenu("MapSpawn")]
     public void MapSpawn()
     {
+        DestroyAllMap();
         UIManager.Instance.ResetContent();
-        Debug.Log(new Vector2(Screen.width / 6, content.sizeDelta.y / 9));
         foreach (var data in mapSpawndata.mapDatasList)
         {
             RectTransform startMap = Instantiate(data.StartMap, content.transform);
             startMap.position = data.StartMapPos;
 
-            Vector2 mapInterval = new Vector2(Screen.width / 6, content.sizeDelta.y / 9);
 
             for (int verti = 1; verti <= mapMaxVertiIndex; verti++)
             {
@@ -41,16 +43,26 @@ public class MapSpawnSystem : MonoBehaviour
                     if (IsFirstVertiIndex())
                     {
                         RectTransform battleMap = Instantiate(data.MapPrefab[0], content.transform);
-                        battleMap.position = new Vector2(/*battleMap.position.x - data.MapInterval.width * horizon, data.StartMapPos.y - data.MapInterval.height * verti*/mapInterval.x * -horizon, mapInterval.y * -verti);
+                        battleMap.position = new Vector2(mapInterval.x * -horizon, mapInterval.y * -verti);
                     }
                     RectTransform spawnMap = Instantiate(data.MapPrefab[randomMap], content.transform);
-                    spawnMap.position = new Vector2(/*spawnMap.position.x - data.MapInterval.width * horizon, -data.StartMapPos.y - (data.MapInterval.height * (verti + 1))*/mapInterval.x * -horizon, mapInterval.y * -verti);
+                    spawnMap.position = new Vector2(mapInterval.x * -horizon, mapInterval.y * -verti);
                 }
                 mapVertiIndex++;
             }
 
             RectTransform bossMap = Instantiate(data.BossMap, content.transform);
             bossMap.position = data.BossMapPos;
+        }
+    }
+
+    public void DestroyAllMap()
+    {
+        GameObject[] maps = GameObject.FindGameObjectsWithTag("Map");
+
+        foreach (var map in maps)
+        {
+            Destroy(map);
         }
     }
 
